0e34dbf3f0b57b39e0061ecbcf368d666c603b32
Sam <samxe94@gmail.com>
2020-11-18 21:58:36 -0500

    ios/andriod compatibility

--- zcomponents/Landing/Landing.jsx
+++ zcomponents/Landing/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-const { Alert, View, Text, Image, StyleSheet, Button } = require("react-native");
+const { Alert, View, Text, Image, StyleSheet, Button, Platform } = require("react-native");
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import logo from './peach.png'
 
@@ -9,8 +9,8 @@ function Landing() {
     return(
         <View style={styles.container}>
             <Image 
-                source={{ uri: logo }}   
-                style={styles.logo}
+                source={require( './peach.png' )}   
+                style={{width: 60, height: 60}}
             />
             <Text style={styles.title}>
                 boodi
@@ -27,23 +27,31 @@ function Landing() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundImage: 'linear-gradient(#FFC0B4, #FFCCB4, #FFD8B4)',
+      backgroundColor: '#FFC0B4',
+      //backgroundImage: 'linear-gradient(#FFC0B4, #FFCCB4, #FFD8B4)',
       alignItems: 'center',
       justifyContent: 'center',
-      display: 'container',
+      display: 'flex',
       flexDirection: 'column'
     },
     title: {
       color: '#5AA382',
       fontSize: RFValue(40, 580),
-      fontFamily: 'Montserrat, sans-serif',
+      //fontFamily: 'Montserrat, sans-serif',
+      //fontFamily: Platform.OS === 'ios' ? 'sans-serif': '',
+      ...Platform.select({
+          ios: {
+              fontFamily: 'GeezaPro-Bold'
+          },
+          android: {
+              fontFamily: 'Roboto'
+          },
+          default: {
+              fontFamily: 'Arial'
+          }
+      }),
       fontWeight: 'bold',
-      display: 'inline-flex',
-      marginBottom: '50px', 
-    },
-    logo: {
-        width: '60px',
-        height: '60px'
+      display: 'flex',
     }
 });
   
